010a3b471b460911e524efd1307fcddfda778185
Andy Sukowski-Bang <dev@andy-sb.com>
2023-10-22 23:38:54 +0200

    Add LSTM forward!() pass

--- layers/lstm.jl
+++ layers/lstm.jl
@@ -21,3 +21,17 @@ function LSTM(dims::Pair{Int, Int})
 		Vector{Float64}(undef, dims[2])
 	)
 end
+
+# forward pass of LSTM cell, return hidden state as output
+function forward!(lstm::LSTM, input::Vector{Float64})::Vector{Float64}
+	input_concat = [input; lstm.hidden_state]
+
+	f = forward!(lstm.forget_gate,   input_concat)
+	i = forward!(lstm.input_gate,    input_concat)
+	o = forward!(lstm.output_gate,   input_concat)
+	n = forward!(lstm.new_candidate, input_concat)
+
+	lstm.cell_state = f .* lstm.cell_state + i .* n
+	lstm.hidden_state = o .* tanh(lstm.cell_state)
+	return lstm.hidden_state
+end
