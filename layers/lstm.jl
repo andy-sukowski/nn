a96c00df8a6e5e61eddbd367517c11b4ec3bcf9a
Andy Sukowski-Bang <dev@andy-sb.com>
2024-01-13 23:47:51 +0100

    Cache LSTM internals per time step for BPTT

--- layers/lstm.jl
+++ layers/lstm.jl
@@ -2,36 +2,45 @@
 
 # long short-term memory cell
 mutable struct LSTM <: Layer
-	forget_gate  ::Dense
-	input_gate   ::Dense
-	output_gate  ::Dense
-	new_candidate::Dense
+	forget_gate      ::Dense
+	input_gate       ::Dense
+	output_gate      ::Dense
+	new_candidate    ::Dense
 
-	cell_state   ::Vector{Float64}
-	hidden_state ::Vector{Float64}
+	forget_gate_out  ::Vector{Vector{Float64}}
+	input_gate_out   ::Vector{Vector{Float64}}
+	output_gate_out  ::Vector{Vector{Float64}}
+	new_candidate_out::Vector{Vector{Float64}}
+
+	cell_state       ::Vector{Vector{Float64}}
+	hidden_state     ::Vector{Vector{Float64}}
 end
 
-function LSTM(dims::Pair{Int, Int})
-	LSTM(
+function LSTM(dims::Pair{Int, Int}; t::Int)::LSTM
+	return LSTM(
 		Dense(sum(dims) => dims[2]; act=σ, act′=σ′),
 		Dense(sum(dims) => dims[2]; act=σ, act′=σ′),
 		Dense(sum(dims) => dims[2]; act=σ, act′=σ′),
 		Dense(sum(dims) => dims[2]; act=tanh, act′=tanh′),
-		Vector{Float64}(undef, dims[2]),
-		Vector{Float64}(undef, dims[2])
+		[Vector{Float64}(undef, dims[2]) for _ in 1:t],
+		[Vector{Float64}(undef, dims[2]) for _ in 1:t],
+		[Vector{Float64}(undef, dims[2]) for _ in 1:t],
+		[Vector{Float64}(undef, dims[2]) for _ in 1:t],
+		[zeros(dims[2]) for _ in 1:t+1],
+		[zeros(dims[2]) for _ in 1:t+1]
 	)
 end
 
 # forward pass of LSTM cell, return hidden state as output
-function forward!(lstm::LSTM, input::Vector{Float64})::Vector{Float64}
-	input_concat = [input; lstm.hidden_state]
+function forward!(l::LSTM, input::Vector{Float64}; t::Int)::Vector{Float64}
+	input_concat = [input; l.hidden_state[t]]
 
-	f = forward!(lstm.forget_gate,   input_concat)
-	i = forward!(lstm.input_gate,    input_concat)
-	o = forward!(lstm.output_gate,   input_concat)
-	n = forward!(lstm.new_candidate, input_concat)
+	l.forget_gate_out[t]   = forward!(l.forget_gate,   input_concat)
+	l.input_gate_out[t]    = forward!(l.input_gate,    input_concat)
+	l.output_gate_out[t]   = forward!(l.output_gate,   input_concat)
+	l.new_candidate_out[t] = forward!(l.new_candidate, input_concat)
 
-	lstm.cell_state = f .* lstm.cell_state + i .* n
-	lstm.hidden_state = o .* tanh(lstm.cell_state)
-	return lstm.hidden_state
+	l.cell_state[t+1]   = l.forget_gate_out[t] .* l.cell_state[t] + l.input_gate_out[t] .* l.new_candidate_out[t]
+	l.hidden_state[t+1] = l.output_gate_out[t] .* tanh.(l.cell_state[t+1])
+	return l.hidden_state[t+1]
 end
